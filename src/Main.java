178d863b36940cbc38d9f41d3a213f0a9b032da8
SergeyA2022 <98774811+SergeyA2022@users.noreply.github.com>
2022-08-13 01:56:52 +0400

    Revert "беда был пьян"
    This reverts commit 29b89f772eb5b3563fe25691745c9427fbc0199b.

--- src/Main.java
+++ src/Main.java
@@ -1,21 +1,21 @@
-//public class Main {
-//
-//    public static void main(String[] args) {
-//        System.out.println("Hello world!");
-//        System.out.println("Hello Anton!");
-//        System.out.println("hjfvghil;km,");
-//        System.out.println("Serega fetch it!");
-//        int res = 0;
-//        int fac = 1;
-//        for (int i = 0; i < 11; i++) {
-//            res += i;
-//            System.out.println(res);
-//        }
-//        System.out.println(res);
-//        for (int i = 1; i < 11; i++) {
-//            fac *= i;
-//        }
-//        System.out.println(fac);
-//        System.out.println(2+2);
-//    }
-//}
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        System.out.println("Hello Anton!");
+        System.out.println("hjfvghil;km,");
+        System.out.println("Serega fetch it!");
+        int res = 0;
+        int fac = 1;
+        for (int i = 0; i < 11; i++) {
+            res += i;
+            System.out.println(res);
+        }
+        System.out.println(res);
+        for (int i = 1; i < 11; i++) {
+            fac *= i;
+        }
+        System.out.println(fac);
+        System.out.println(2+2);
+    }
+}
